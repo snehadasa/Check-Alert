c26db1505bc61610e56f07e8bca542f451fada49
David Kwan <dwkwan1989@gmail.com>
2019-10-04 21:11:12 +0000

    Add logic for projects with no checker and refactor logic for monitoring checker status

--- test1.py
+++ test1.py
@@ -6,6 +6,7 @@ import getpass
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
+
 id = 0
 def get_user_info():
   key = input('\nVisit: https://intranet.hbtn.io/dashboards/my_tools\nand enter your API Key from the bottom of the page: \n')
@@ -21,31 +22,41 @@ def get_user_info():
   return key, user_id, psw, project
 key, user_id, psw, project = get_user_info()
 email = user_id + '@holbertonschool.com'
-user_data = {'api_key':key, 'email': email, 'password': psw, "scope": "checker"}
+user_data = {'api_key':key, 'email': email, 'password': psw, 'scope': "checker"}
 url = 'https://intranet.hbtn.io/users/auth_token.json'
 auth = requests.post(url=url, data=user_data)
 print(auth.json())
 print()
 token = auth.json().get('auth_token')
+url_p = 'https://intranet.hbtn.io/projects/' + project  + '.json?auth_token=' + token
+pj = requests.get(url=url_p)
+i = 0
+flag = 0
+while (i < len(pj.json().get('tasks'))):
+  if pj.json().get('tasks')[i].get('checker_available') is True:
+    flag = 1
+    task_id = pj.json().get('tasks')[i].get('id')
+if flag == 0:
+  print("No checker for project")
+  exit
 
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=10)
 def timed_job():
-  url_p = 'https://intranet.hbtn.io/projects/' + project  + '.json?auth_token=' + token
-  pj = requests.get(url=url_p)
-
-  i = 0
-  while (i < len(pj.json().get('tasks'))):
-    if pj.json().get('tasks')[i].get('checker_available') is True:
-      checkers = "Checkers is out"
-      payload={"text": checkers}
-      requests.post('https://hooks.slack.com/services/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', data=json.dumps(payload), headers={'Content\
--Type': 'application/json'} )
-      exit()
-    i += 1
-  checkers = "Only manual for " + pj.json().get('name')
+  url_request = 'https://intranet.hbtn.io/tasks/' + task_id + 'start_correction.json' + '.json?auth_token=' + token
+  request_dict = requests.post(url=url_request).json()
+  request_id = request_dict.get('id')
+  if request_id != 0:
+    checkers = "Checker is out"
+    payload={"text": checkers}
+    requests.post('https://hooks.slack.com/services/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', data=json.dumps(payload), headers={\
+'Content\
+    -Type': 'application/json'} )
+    exit()
+  checkers = "Checkers for " + pj.json().get('name') + " is not available"
   payload={"text": checkers}
-  requests.post('https://hooks.slack.com/services/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', data=json.dumps(payload), headers={'Content-Type': 'application/json'} )
+  requests.post('https://hooks.slack.com/services/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', data=json.dumps(payload), headers={'C\
+ontent-Type': 'application/json'} )
 
 # sched.configure(options_from_ini_file)
 sched.start()
