019b26f2bc54a6e905f03aa41c6be862f41e1f9b
Thomas Graeff <676@holbertonschool.com>
2019-10-04 22:28:50 +0000

    Add () to exit

--- test1.py
+++ test1.py
@@ -45,7 +45,7 @@ while (i < len(pj.json().get('tasks'))):
   i += 1
 if flag == 0:
   print("No checker for project")
-  exit
+  exit()
 
 sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=10, id='stop')
